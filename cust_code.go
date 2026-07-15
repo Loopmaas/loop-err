b7d844298a0f65cf52662d31f315154f1350a543
vincent <vincent@loopmaas.com>
2026-07-15 15:28:56 +0800

    NegativeRatingMustTagOrText

--- cust_code.go
+++ cust_code.go
@@ -83,7 +83,7 @@ const (
 
 	// Rating
 	RatingStatusTransitionInvalid = 400_13_001 // 評價狀態轉換不合法
-
+	NegativeRatingMustTagOrText   = 400_13_002 // 負面評價至少需要Tag或文字
 	// 401 status code from here
 	Unauthorized = 401_0000 // 未授權
 	// UnusualLogin       = 401_0001 // 登入異常
